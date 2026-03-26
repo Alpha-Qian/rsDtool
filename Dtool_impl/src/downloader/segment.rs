79c6d28d227f0cac64f815b1f97ba051379c14fc
AlphaQian <154665575+Alpha-Qian@users.noreply.github.com>
2026-03-27 00:34:38 +0800

    segment cache

--- Dtool_impl/src/downloader/segment.rs
+++ Dtool_impl/src/downloader/segment.rs
@@ -1,30 +1,30 @@
 //!持久化
 
-use std::sync::Arc;
-
-use crate::downloader::request::DownloadRequest;
+use std::{error::Error, fmt::{Debug, Display}, mem, sync::Arc};
+use std::num::NonZeroU64;
+use crate::downloader::httprequest::RequestInfo;
 
 
 
 pub struct ResumeInfo{
-    request: DownloadRequest,
+    request: RequestInfo,
     segments: Vec<Segment>
 }
 
 
-pub trait CodecSegment{//S 是ProgreaaSender //simple
-    type ProcessSender;
-    ///加载进度并启动
-    fn load_segment(segment: Segment) -> (self, Self::ProcessSender);
+// pub trait CodecSegment{//S 是ProgreaaSender //simple
+//     type ProcessSender;
+//     ///加载进度并启动
+//     fn load_segment(segment: Segment) -> (self, Self::ProcessSender);
 
-    ///中途保存进度
-    fn save_as_segment(&self) -> Segment;
-}
+//     ///中途保存进度
+//     fn save_as_segment(&self) -> Segment;
+// }
 
 
-struct Segments{
-    inner: Vec<Segment>
-}
+// struct Segments{
+//     inner: Vec<Segment>
+// }
 
 #[derive(Clone)]
 pub struct Segment{
@@ -33,27 +33,22 @@ pub struct Segment{
 }
 
 impl Segment {
+
+    pub(crate) fn new(remain: u64, end: u64) -> Self{
+        Self { remain, end }
+    }
+
     pub fn full(size: u64) -> self{
         Self{remain: size, end: size}
     }
-}
 
+    pub fn start(&self) -> u64{
+        self.end - self.remain
+    }
 
-
-pub struct SplitSegment{
-    remain: u64,
-    end: u64
-}
-
-impl SplitSegment{
-    pub fn next_Segment(&mut self, len: u64) ->Option<Segment> {
-        if self.remain > len {
-            Some(Segment { remain: len, end })
-        } else if self.remain > 0 {
-            Some(())
-        } else {
-            None
-        }
+    pub fn split_at(self, first_remain: u64) -> (Self, Option<Self>) {
+        let mut iter = self.split_by_step(first_remain);
+        (iter.next().unwrap(), iter.try_into::<Self>().ok())
     }
 
     pub fn split_by_times(self, times: usize) -> SegmentIter{
@@ -65,24 +60,83 @@ impl SplitSegment{
     }
 }
 
-
 pub struct SegmentIter{
+    step: u64,
+
+    //
     remain: u64,
     end: u64,
-    step: u64,
 }
 
 impl Iterator for SegmentIter {
     type Item = Segment;
     fn next(&mut self) -> Option<Self::Item> {
         if self.remain > self.step {
-            Some(Segment { remain: len, end })
+
+            self.remain -= self.step;
+            Segment::new(self.step, self.start() ).into()
+
         } else if self.remain > 0 {
-            Some(())
+
+            self.remain = 0;//迭代器耗尽
+            Segment::new(remain, end).into()
+
         } else {
             None
         }
     }
 }
 
+impl TryFrom<SegmentIter> for Segment{
+    type Error = FromSegmentIterError;
+    fn try_from(value: SegmentIter) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
 
+impl SegmentIter {
+    fn new(step: u64, remain: u64, end: u64) -> Self{
+        Self { step, remain, end }
+    }
+
+    fn start(&self) -> u64{
+        self.end - self.remain
+    }
+}
+
+
+impl From<FromSegmentIterError> for CreateSegmentError {
+    fn from(value: FromSegmentIterError) -> Self {
+        Self::ZeroRemain
+    }
+}
+
+#[derive(Debug)]
+struct FromSegmentIterError;
+
+impl Display for FromSegmentIterError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&"无法转换已耗尽迭代器")
+    }
+}
+impl Error for FromSegmentIterError{}
+
+
+#[derive(Debug)]
+enum CreateSegmentError {
+    ZeroRemain,
+    NegtiveStartPoint
+}
+
+
+// impl Debug for  {
+    
+// }
+impl Display for CreateSegmentError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match Self {
+            Self::ZeroRemain => todo!(),
+            Self::NegtiveStartPoint => todo!()
+        }
+    }
+}
